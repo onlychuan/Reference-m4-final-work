0c03320366c3572a91a0fb73c3726df1bd56c178
allen <onlychuan95@gmail.com>
2020-03-22 00:46:45 +1000

    UPDATE MODEL DATA

--- Reference/Model/Cat.swift
+++ Reference/Model/Cat.swift
@@ -9,18 +9,28 @@
 import Foundation
 
 //defind for cat breed, model base class
-struct Cat: Identifiable{
+class Cat: ObservableObject, Identifiable{
     var id:Int
     var img:String
     var name:String
     var breed:String
     var color:String
     var size: String
+    @Published var note:String
+    init(id:Int,img:String,name:String,breed:String,color:String,size:String,note:String) {
+        self.id = id
+        self.img = img
+        self.name = name
+        self.breed = breed
+        self.color = color
+        self.size = size
+        self.note = note
+    }
 }
 
 //cat data detail
 let Data = [
-    Cat(id: 002, img:"cat1", name:"litte A", breed:"Devon Rex", color:"Black", size:"Small"),
-    Cat(id: 003, img:"cat2", name:"John", breed:"Sphynx", color:"White", size:"Small"),
-    Cat(id: 001, img:"cat", name:"Lily", breed:"English short hair", color:"Tabby", size:"Large"),
+    Cat(id:001, img:"cat1", name:"litte A", breed:"Devon Rex", color:"Black", size:"Small",note:""),
+    Cat(id: 003, img:"cat2", name:"John", breed:"Sphynx", color:"White", size:"Small",note:""),
+    Cat(id: 002, img:"cat", name:"Lily", breed:"English short hair", color:"Tabby", size:"Large",note:""),
 ]
